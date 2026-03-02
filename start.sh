5452da79c089b5ef227bcf330a650b52a71c528e
venghatesh-stack <venghatesh@gmail.com>
2026-03-02 20:45:35 +0530

    gunicorn startup script updated

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Starting on port: $PORT"
-exec gunicorn app:app --bind 0.0.0.0:$PORT --workers 1 --threads 2 --timeout 120 --log-level debug
+exec gunicorn "app:create_app()" --bind 0.0.0.0:$PORT --workers 1 --threads 2 --timeout 120 --log-level debug
