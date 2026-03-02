dcd109dfa47a806bdd3763ae6854c39c268b85d5
venghatesh-stack <venghatesh@gmail.com>
2026-03-02 21:16:17 +0530

    Update login_service.py

--- services/login_service.py
+++ services/login_service.py
@@ -1,14 +1,11 @@
 from functools import wraps
 from flask import session, redirect, url_for
-
-
-
 def login_required(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
         # 🔒 Require BOTH flags
         if not session.get("authenticated") or "user_id" not in session:
             session.clear()
-            return redirect(url_for("login"))
+            return redirect(url_for("planner.login"))
         return fn(*args, **kwargs)
     return wrapper
