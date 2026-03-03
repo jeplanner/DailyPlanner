b13ae5e6782a1179148b288b39b064c56e737296
venghatesh-stack <venghatesh@gmail.com>
2026-03-03 20:21:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from werkzeug.exceptions import HTTPException
 
 def create_app():
     app = Flask(__name__)
-
+    from werkzeug.middleware.proxy_fix import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
     # ----------------------------
     # Core Configuration
     # ----------------------------
